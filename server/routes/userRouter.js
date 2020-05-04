791aca39ade092e5072c2126c9f15872b43e30d5
tanvirgeek <tanvirgeek@gmail.comm>
2020-05-04 08:04:22 +0600

    JWT token Tutorial 3 in the middle

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const UserModel = require("../models/Usermodel");
+const auth = require("../middlewares/auth");
+const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
 
 router.post('/register', async (req, res) => {
     try {
-        const { email, password, displayName } = req.body;
+        let { email, password, displayName } = req.body;
+
+        //vallidate
         if (!email || !password) {
             return res.status(400).json({ message: "Not all fields have been entered" });
         }
@@ -13,8 +17,12 @@ router.post('/register', async (req, res) => {
         if(existingUser){
             return res.status(400).json({msg:"an account with that email already exists"})
         }
+
+        //generate passwordHash
         const salt = await bcrypt.genSalt();
         const passwordHash = await bcrypt.hash(password,salt);
+
+        //create and save user
         const newUser = new UserModel({
             email,
             password: passwordHash,
@@ -22,10 +30,50 @@ router.post('/register', async (req, res) => {
         });
         const savedUser = await newUser.save();
         res.json(savedUser);
+
     }catch(err){
         res.status(500).json({error: err})
     }
-    
+      
+})
+
+//login
+router.post("/login", async (req, res)=>{
+    try{
+
+        const {password, email} = req.body;
+        //validate
+        if(!email || !password){
+            return res.status(400).json({ message: "Not all fields have been entered" });
+        }
+
+        //check if login correct
+        const user = await UserModel.findOne({email:email})
+        if(!user){
+            return res.status(400).json({msg:"No account with that email address"});
+        }
+
+        const ismatch = await bcrypt.compare(password, user.password );
+        if (!ismatch) 
+            return res.status(400).json({msg:"invalid Credentials"});
+
+        const token = jwt.sign({id: user._id}, process.env.jwt_SECRET);
+        res.json({
+            token,
+            user:{
+                id: user._id,
+                email: user.email
+            }
+        })
+
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+})
+
+//delete Account
+router.delete('/delete', auth ,async(req, res)=>{
+
 })
 
 module.exports = router;
